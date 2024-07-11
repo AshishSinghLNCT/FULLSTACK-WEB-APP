673662566712d93bcb0c76fded10f441f03286fe
Ashish Singh <ashishzoom1991@gmail.com>
2024-07-11 09:38:31 +0530

    Update urls.py

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from mysite import app
 from .views import UserViewSet, InterestViewSet, ChatMessageViewSet
 
 router = DefaultRouter()
@@ -10,4 +12,16 @@ router.register(r'chatmessages', ChatMessageViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
+from django.urls import path, include
+from rest_framework.authtoken import views as drf_views
+
+urlpatterns = [
+    ...,
+    path('api/auth/', include('rest_framework.urls')),
+    path('api/auth/token/', drf_views.obtain_auth_token),
+    path('api/register/', app.views.RegisterView.as_view()),  # custom registration view
+...
+]
+
+
 
